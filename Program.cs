a7243d96a6077120b265e568a4b2f07de9d852dd
Johan Myburgh <myburgh@gmail.com>
2023-04-12 13:08:56 +0100

    Not sure

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.Logging.ApplicationInsights;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 
 var app = builder.Build();
 
