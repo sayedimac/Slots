747338cfee79e4cf7ba9e229c5b85aee2dd2418d
Johan Myburgh <myburgh@gmail.com>
2023-11-14 15:08:44 +0000

    about us

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,10 @@ public class HomeController : Controller
     {
         return View();
     }
+        public IActionResult About()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
