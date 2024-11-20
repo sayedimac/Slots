269eebe1baf1df28c7950164d632f08a9ce3d7f2
Johan Myburgh <myburgh@gmail.com>
2024-11-20 15:02:46 +0000

    asdasdadasdsad

--- obj/Debug/net6.0/slots.AssemblyInfo.cs
+++ obj/Debug/net6.0/slots.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("slots")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d77d827ab1c0b1c963913395010eb00173aef3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("slots")]
 [assembly: System.Reflection.AssemblyTitleAttribute("slots")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
